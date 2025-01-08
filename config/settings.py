b7e847b59f0e148dd2ad77f3e04ab51a2130da60
bgonzales <bgonzales@minsait.com>
2025-01-08 15:43:02 +0100

    Add broken authentication

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'  # Página a la que redirige después de login
+LOGOUT_REDIRECT_URL = '/login/'  # Página a la que redirige después de logout
