aa4665a054431d82f88855dde3ab105ace0d2547
Bladimir Gonzales <bgonzales@minsait.com>
2025-01-29 22:30:57 +0100

    add migrations

--- init.sql
+++
@@ -1,3 +0,0 @@
--- init.sql
-INSERT INTO resources.resource (name, description,available) VALUES ('aula', 'aula', true);
-INSERT INTO resources.resource (name, description,available) VALUES ('aula', 'aula', true);
