f8558ed4c68f4958925985af2a38614094c02665
Dart5045 <dart5045@gmail.com>
2025-02-04 21:07:52 +0100

    Add design

--- classrooms/forms.py
+++ classrooms/forms.py
@@ -5,3 +5,8 @@ class ClassroomForm(forms.ModelForm):
     class Meta:
         model = Classroom
         fields = ['name', 'capacity', 'location']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),  # Campo de texto con clase 'form-control'
+            'capacity': forms.NumberInput(attrs={'class': 'form-control'}),  # Campo de número con 'form-control'
+            'location': forms.TextInput(attrs={'class': 'form-control'}),  # Campo de texto con 'form-control'
+        }
