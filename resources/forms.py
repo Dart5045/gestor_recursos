21457fa204712562f314d232de1da7a57eb1cfc9
Dart5045 <dart5045@gmail.com>
2025-02-04 21:07:52 +0100

    Add design

--- resources/forms.py
+++ resources/forms.py
@@ -5,3 +5,7 @@ class ResourceForm(forms.ModelForm):
     class Meta:
         model = Resource
         fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+        }
