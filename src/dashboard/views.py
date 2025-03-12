27b3f6e5cc49cafe9ad127966b16fc754c1c734a
Bladimir Gonzales <dart5045@gmail.com>
2025-03-12 22:43:19 +0100

    Fix login errors

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -7,3 +7,4 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def index(request):
     return render(request, 'dashboard/index.html')
+
--- src/login/views.py
+++ src/login/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.utils.timezone import now
 from .documents import UserDocument
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+
+
 
 def log_login_attempt(username, success, ip_address):
     """ Registra intentos de login (exitosos o fallidos) en Elasticsearch """
@@ -19,40 +24,23 @@ def login_form(request):
 
 def vulnerable_login(request):
     ip_address = request.META.get('REMOTE_ADDR')
-
     if request.method == 'POST':
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
-        user = authenticate(request, username=username, password=password)
-
-        success = user is not None  # True si la autenticación fue correcta
-
-        # Registrar intento de login en Elasticsearch
-        log_login_attempt(username, success, ip_address)
-
-        if success:
-            login(request, user)
-            return redirect('/')
-        else:
-            return HttpResponse('Credenciales inválidas')
-
+        try:
+            user = User.objects.get(username=username)
+            # No se recomienda guardar contraseñas en texto plano
+            if user.password == password:
+                login(request, user)  # Llama al método login correctamente
+                messages.success(request, 'Inicio de sesión exitoso.')
+                # Registrar intento de login en Elasticsearch
+                #log_login_attempt(username, success, ip_address)
+                return redirect('dashboard') 
+        except User.DoesNotExist:
+            # Error de autenticación
+            messages.error(request, 'Usuario o contraseña incorrectos.')
     return render(request, 'login/vulnerable_login.html')
 
 def custom_logout(request):
     logout(request)
     return render(request, 'login/logout.html')
-
-def custom_login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    try:
-        user = User.objects.get(username=username)
-        # No se recomienda guardar contraseñas en texto plano
-        if user.password == password:
-            login(request, user)  # Llama al método login correctamente
-            messages.success(request, 'Inicio de sesión exitoso.')
-            return redirect('dashboard') 
-    except User.DoesNotExist:
-        # Error de autenticación
-        messages.error(request, 'Usuario o contraseña incorrectos.')
-    return render(request, 'dashboard/login_form.html')
